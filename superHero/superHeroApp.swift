b230428411587797dcef44c650fd0e8d296feb80
Waleed Ahmed <waleed.wit@gmail.com>
2022-07-09 02:12:43 +0300

    Initial Commit

---
+++ superHero/superHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  superHeroApp.swift
+//  superHero
+//
+//  Created by Waleed on 09/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct superHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
